a71d51dbb21b8693594101436a1e6cba04ceb3a8
a1ecsss <estebana.delapena@gmail.com>
2025-01-17 12:11:00 -0600

    Primer commit

---
+++ IBlender.java
@@ -0,0 +1,8 @@
+public interface IBlender{
+    abstract public void SpeedUp();
+    abstract public boolean IsFull();
+    abstract public void Fill();
+    abstract public void Empty();
+    abstract public void SpeedDonw();
+    abstract public int GetSpeed();
+}
