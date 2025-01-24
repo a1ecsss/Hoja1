7674478381c9cc2068d0bcb6a861823983e611c1
a1ecsss <estebana.delapena@gmail.com>
2025-01-23 22:30:43 -0600

    Implementacion de las funciones de la licuadora

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        IBlender blender = new Blender();
         int opcion;
         
         do {
@@ -20,20 +21,28 @@ public class Main {
 
             switch (opcion) {
                 case 1:
+                    blender.Fill();
                     break;
                 case 2:
+                    blender.Fill();
                     break;
                 case 3:
+                    blender.SpeedUp();
                     break;
                 case 4:
+                    System.out.println("Velocidad actual: " + blender.GetSpeed());
                     break;
                 case 5:
+                    System.out.println(blender.IsFull() ? "La licuadora está llena." : "La licuadora está vacía.");
                     break;
                 case 6:
+                    blender.Empty();
                     break;
                 case 7:
+                    System.out.println("Saliendo del programa...");
                     break;
                 default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
             }
         } while (opcion != 7);
         
